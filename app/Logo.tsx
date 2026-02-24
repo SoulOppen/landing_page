3a22ebf82e5cdac0bdd9990627841fdc205d4f89
souloppen <arieloppenheimer@outlook.cl>
2026-02-24 12:51:16 -0300

    add:shadow efect

--- app/Logo.tsx
+++ app/Logo.tsx
@@ -6,14 +6,14 @@ type LogoProps = {
   classAdd?:string
 };
 
-export default function Logo({ size,classAdd }: LogoProps) {
+export default function Logo({ size, classAdd }: LogoProps) {
   return (
     <Image
       src={logo}
       alt="Logo"
       width={size}
       height={size}
-      className={`${classAdd} rounded-full m-auto`}
+      className={`${classAdd} rounded-full m-auto filter sepia 150 shadow-[0_10px_30px_rgba(236,72,153,0.6)]`}
     />
   );
 }
